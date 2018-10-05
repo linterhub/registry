2d456674902fdfd0babfdaf156a2ac268218b28a
Oleg Safonov <doroxz@protonmail.com>
2018-10-05 18:56:50 +0300

    feat: add gem registry

---
+++ src/registry/gem.ts
@@ -0,0 +1,49 @@
+import { Dependency } from '../model/data';
+import { Template, Arguments, Source } from '../model/registry';
+
+const api: Template = {
+    name: 'gem',
+    repositories: [
+        'rubygems.org'
+    ],
+    urlPrototype: 'https://{repository}/api/v1/gems/{name}.json`',
+    requests: {
+        "meta": {
+            converter: (json: any, args: Arguments & Source) => {
+                return {
+                    name: json.name,
+                    description: json.description,
+                    url: json.homepage_uri,
+                    license: json.licenses ? json.licenses[0] : null,
+                    version: json.version,
+                };
+            }
+        },
+        "deps": {
+            converter: (json: any, args: Arguments & Source) => {
+                const dependencies: Dependency[] = [];
+
+                for (const key in json.dependencies.runtime) {
+                    if (json.dependencies.runtime.hasOwnProperty(key)) {
+                        dependencies.push({
+                            source: {
+                                registry: args.registry,
+                                repository: args.repository
+                            },
+                            package: json.dependencies.runtime[key].name,
+                            version: json.dependencies.runtime[key].requirements
+                        });
+                    }
+                }
+
+                return dependencies;
+            }
+        },
+        "versions": {
+            urlPrototype: 'https://{repository}/api/v1/versions/{name}.json`',
+            converter: (json: any) => json
+        },
+    }
+};
+
+export = api;
